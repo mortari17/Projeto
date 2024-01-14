e9ba12caa93d0a610184c49b57bd80b94b54c202
Matheus Mortari <matheus.mortari@prudentte.com.br>
2024-01-14 18:07:49 -0300

    fix: avatar link

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
-  const html = document.documentElement /*documentElement é o html*/
+  const html = document.documentElement; /*documentElement é o html*/
   /* if (html.classList.contains("light")) {
     html.classList.remove("light")
   } else {
     html.classList.add("light")
   }*/
-  html.classList.toggle("light")
+  html.classList.toggle("light");
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png");
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png");
   }
 }
